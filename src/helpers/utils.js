b80105a81c16a6eb2c92d12e80c654f67eb7ade8
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-11-05 15:29:20 +0700

    add apiLive

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,6 +1,6 @@
-const apiLive = ""
+const apiLive = "https://vWork.vnfite.com.vn"
 const apiTest = "http://192.168.100.178:2345"
 
-const BASE_URL = apiTest
+const BASE_URL = apiLive
 
 export default {BASE_URL}
