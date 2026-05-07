4744bd8527f21813c6aa206ed41d06e92b8630f4
Phùng Chí Kiên <phungchikien196@gmail.com>
2026-05-07 13:15:58 +0700

    Xóa file thừa

--- src/navigators/stack/CRMStackNavigator.js
+++ src/navigators/stack/CRMStackNavigator.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CRMBottomTab from "../bottomtabs/CRMBottomTab";
-import AgencyScreen from "../../screens/crm/AgencyScreen";
 // import WorkDetailScreen from "../screens/workplace/WorkDetailScreen";
 // import WorkSettingsScreen from "../screens/workplace/WorkSettingsScreen";
 import ListAgentScreen from "../../screens/crm/ListAgentScreen";
@@ -12,7 +11,6 @@ export default function CRMStackNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="CRMBottomTab" component={CRMBottomTab} />
-      <Stack.Screen name="AgencyScreen" component={AgencyScreen} />
       <Stack.Screen name="ListAgentScreen" component={ListAgentScreen} />
 
     </Stack.Navigator>
